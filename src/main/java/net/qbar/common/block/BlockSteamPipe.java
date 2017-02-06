e20689f9ef2cfc33d80cbadde37e098c4d2fbeab
Phenix246 <florian.gomes246@gmail.com>
2017-02-06 01:27:36 +0100

    Add clean bounding box for steam pipe

--- src/main/java/net/qbar/common/block/BlockSteamPipe.java
+++ src/main/java/net/qbar/common/block/BlockSteamPipe.java
@@ -6,6 +6,7 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -18,6 +19,14 @@ import net.qbar.common.tile.TileSteamPipe;
 
 public class BlockSteamPipe extends BlockMachineBase
 {
+    protected static final AxisAlignedBB AABB_NONE  = new AxisAlignedBB(0.35D, 0.25D, 0.35D, 0.65D, 0.55D, 0.65D);
+    protected static final AxisAlignedBB AABB_EAST  = new AxisAlignedBB(0.65D, 0.25D, 0.35D, 1.00D, 0.55D, 0.65D);
+    protected static final AxisAlignedBB AABB_WEST  = new AxisAlignedBB(0.00D, 0.25D, 0.35D, 0.35D, 0.55D, 0.35D);
+    protected static final AxisAlignedBB AABB_SOUTH = new AxisAlignedBB(0.65D, 0.25D, 0.35D, 0.65D, 0.55D, 1.00D);
+    protected static final AxisAlignedBB AABB_NORTH = new AxisAlignedBB(0.35D, 0.25D, 0.00D, 0.35D, 0.55D, 0.35D);
+    protected static final AxisAlignedBB AABB_UP    = new AxisAlignedBB(0.35D, 0.55D, 0.35D, 0.65D, 1.00D, 0.65D);
+    protected static final AxisAlignedBB AABB_DOWN  = new AxisAlignedBB(0.35D, 0.00D, 0.35D, 0.65D, 0.25D, 0.65D);
+
     public BlockSteamPipe()
     {
         super("steampipe", Material.IRON);
@@ -25,6 +34,12 @@ public class BlockSteamPipe extends BlockMachineBase
 
     @Override
     public boolean isOpaqueCube(final IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullBlock(final IBlockState state)
     {
         return false;
     }
@@ -78,5 +93,24 @@ public class BlockSteamPipe extends BlockMachineBase
     public TileEntity createNewTileEntity(final World worldIn, final int meta)
     {
         return new TileSteamPipe(8);
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        AxisAlignedBB res = AABB_NONE;
+        if (source.getTileEntity(pos.east()) instanceof TileSteamPipe)
+            res = res.union(AABB_EAST);
+        if (source.getTileEntity(pos.west()) instanceof TileSteamPipe)
+            res = res.union(AABB_WEST);
+        if (source.getTileEntity(pos.north()) instanceof TileSteamPipe)
+            res = res.union(AABB_NORTH);
+        if (source.getTileEntity(pos.south()) instanceof TileSteamPipe)
+            res = res.union(AABB_SOUTH);
+        if (source.getTileEntity(pos.up()) instanceof TileSteamPipe)
+            res = res.union(AABB_UP);
+        if (source.getTileEntity(pos.down()) instanceof TileSteamPipe)
+            res = res.union(AABB_DOWN);
+        return res;
     }
 }
